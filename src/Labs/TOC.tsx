be5e39ebb115485414a3a4edd80dd440a38672b2
QingyuanWan <wanqingyuan2001@163.com>
2024-10-03 01:44:06 -0400

    add git id

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,35 +1,35 @@
-import { useLocation } from "react-router";
+import { Link, useLocation } from "react-router-dom";
 export default function TOC() {
   const { pathname } = useLocation();
   return (
     <ul className="nav nav-pills">
       <li className="nav-item">
-        <a id="wd-a" href="#/Labs" className="nav-link">
+        <Link id="wd-a" to="/Labs" className="nav-link">
           Labs
-        </a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a id="wd-a1" href="#/Labs/Lab1"
+        <Link id="wd-a1" to="/Labs/Lab1"
           className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
           Lab 1
-        </a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a id="wd-a2" href="#/Labs/Lab2"
+        <Link id="wd-a2" to="/Labs/Lab2"
           className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
           Lab 2
-        </a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a id="wd-a3" href="#/Labs/Lab3"
+        <Link id="wd-a3" to="/Labs/Lab3"
           className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
           Lab 3
-        </a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="#/Kanbas" className="nav-link">
+        <Link id="wd-k" to="/Kanbas" className="nav-link">
           Kanbas
-        </a>
+        </Link>
       </li>
       <li className="nav-item">
         <a id="wd-github" href="https://github.com/QingyuanWan/cs2024fall_webdev" className="nav-link">
