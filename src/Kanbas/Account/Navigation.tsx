01c002550fbe2135feff98e369d2034e51acf7c8
QingyuanWan <wanqingyuan2001@163.com>
2024-10-31 22:51:30 -0400

    change final_2 a4

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
   // const { currentUser } = useSelector((state: any) => state.accountReducer);
